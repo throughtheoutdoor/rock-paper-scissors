0ed98de1dc77d83ca0e73e1cc0afccb9ea2b5bb3
David Deefholts <197536288+throughtheoutdoor@users.noreply.github.com>
2026-05-26 21:44:14 -0600

    Added code to playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
 // returns rock, paper or scissors based
@@ -16,19 +19,40 @@ function getComputerChoice() {
     }
 
     return choice;
-
 }
 
 
-   
 function getHumanChoice() {
 //returns the user's choice
 
-    let uchoice = prompt(`What is your choice?
+    let choice = prompt(`What is your choice?
 rock, paper or scissors`);
     
-    return uchoice;
-
+    return choice;
 }
 
 
+function playRound( humanChoice, computerChoice){
+//determines a winner given the two parameters
+//Rock beats scissors
+//Scissors beats paper
+//Paper beats rock
+
+let result = "";
+let human = humanChoice.toLowerCase();
+let computer = computerChoice.toLowerCase();
+
+if (human == computer) {
+    console.log (`Tie, ${human} matches ${computer}!`);
+} 
+// else if (human == "rock" && computer == "scissors") {
+//     console.log ("You win! Rock beats scissors.");
+// } else  (human == "rock" && computer == "paper"){
+//     console.log ("You lose! Paper covers rock.");
+// 
+}
+
+            
+
+
+playRound("paper", "paper");
